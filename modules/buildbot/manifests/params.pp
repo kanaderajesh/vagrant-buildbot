ff390b0f025e95df8752adc95c49d82045f387cd
rajesh <rajesh>
2016-01-26 17:11:12 +0530

    module concat and stdlib added

---
+++ modules/buildbot/manifests/params.pp
@@ -0,0 +1,28 @@
+# Class: buildbot::params
+# Parameters used throughout the Buildbot module.
+#
+class buildbot::params {
+  if $caller_module_name != $module_name {
+    fail("Use of private class ${name} by ${caller_module_name}")
+  }
+
+  $version = '0.8.9' # buildbot and buildbot-slave version
+
+  $user  = 'buildbot'
+  $group = 'buildbot'
+  $home  = '/home/buildbot'
+  $shell = '/bin/bash'
+
+  case $::osfamily {
+    'Debian': { $prereq_pkgs = [ 'python2.7-dev', 'python-pip' ]   }
+    'RedHat': { $prereq_pkgs = [ 'python-devel', 'python-pip' ] }
+    default:  { fail("Unsupported OS family '${::osfamily}'")   }
+  }
+
+  # Build slave parameters
+  $slave_port        = '9989'
+  $slave_password    = 'Buildbot-slave-pw'
+  $slave_admin       = 'Buildbot Admin <buildbot-admin@example.com>'
+  $slave_description = 'Please put a description of the build host here.'
+  $slave_max_builds  = $::processorcount * 2
+}

ab1b4e26b2ffff25e58c6b5484cac22dd751e4f0
kanaderajesh <kanaderajesh@gmail.com>
2016-01-27 21:08:26 +0530

    added the slave puppet instance

--- manifests/default.pp
+++ manifests/default.pp
@@ -7,4 +7,14 @@ class { '::buildbot::master':
     project_url => 'http://localhost.com',
     slave_port  => '9989',
 }
-Exec['apt-update'] -> Class['::buildbot::master']
+
+class { '::buildbot::slave':
+    master      => '127.0.0.1',
+    master_port => '9989',
+    slave_name  => 'example-slave1',
+    password    => 'pass',
+    admin       => 'Admin <admin@example.com>',
+    description => 'Python build slave.',
+    max_builds  => '4',
+}
+Exec['apt-update'] -> Class['::buildbot::master'] -> Class['::buildbot::slave']
